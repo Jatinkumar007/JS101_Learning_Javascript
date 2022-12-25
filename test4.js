6d40ec58cd2cf2dde9d56a9a148fa079f51639bc
Jatinkumar007 <jatin.kr9868@gmail.com>
2022-12-25 22:35:52 +0530

    Logical_Operators_&_Ternary_Operator

---
+++ test4.js
@@ -0,0 +1,13 @@
+// Given 3 numbers (all different values), print which is greatest
+let a= 12;
+let b= 5;
+let c= 9;
+if(a>b && a>c){
+  console.log("a is greatest")
+}
+else if(b>a && b>c){
+  console.log("b is greatest");
+}
+else if(c>a && c>b){
+  console.log("c is greatest");
+}
